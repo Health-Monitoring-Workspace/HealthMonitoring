3b0ff25b6c0c8c74b399de6a7fe6b4cd8f6eef55
Daniel Burtic <patricia.burtic@gmail.com>
2022-03-15 19:24:50 +0200

    Add alerts to patient data

--- src/main/java/com/example/healthmonitoring/internal/supervisor/service/SupervisorService.java
+++ src/main/java/com/example/healthmonitoring/internal/supervisor/service/SupervisorService.java
@@ -4,6 +4,9 @@ import com.example.healthmonitoring.common.domain.entity.Device;
 import com.example.healthmonitoring.common.domain.entity.EmergencyContact;
 import com.example.healthmonitoring.common.domain.entity.MedicalRecord;
 import com.example.healthmonitoring.common.domain.entity.Patient;
+import com.example.healthmonitoring.common.domain.entity.utility.Alert;
+import com.example.healthmonitoring.common.domain.entity.utility.AlertType;
+import com.example.healthmonitoring.common.domain.entity.utility.Gravity;
 import com.example.healthmonitoring.common.domain.entity.utility.PatientVitalSignsData;
 import com.example.healthmonitoring.common.domain.repository.*;
 import com.example.healthmonitoring.internal.supervisor.dto.PatientDTO;
@@ -19,6 +22,8 @@ import reactor.core.publisher.Mono;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -27,8 +32,6 @@ import java.time.format.DateTimeFormatter;
 public class SupervisorService {
 
     PatientRepository patientRepository;
-
-    EventRepository eventRepository;
 
     EmergencyContactRepository emergencyContactRepository;
 
@@ -54,7 +57,158 @@ public class SupervisorService {
     }
 
     public Flux<PatientVitalSignsData> getPatientsData(@NotNull final SupervisorDTO principal) {
-        return patientRepository.getDataFromMaterializedView(principal.getId());
+        return patientRepository.getDataFromMaterializedView(principal.getId())
+                .flatMap(this::addAlerts);
+    }
+
+    private Mono<PatientVitalSignsData> addAlerts(PatientVitalSignsData data) {
+        List<Alert> alerts = new ArrayList<>();
+//        Target heart rate
+//        moderate-intensity physical activity
+//        For moderate-intensity physical activity, your target heart rate
+//        should be between 64% and 76%1,2 of your maximum heart rate.
+//        You can estimate your maximum heart rate based on your age.
+//        To estimate your maximum age-related heart rate, subtract your age from 220.
+//        For example, for a 50-year-old person, the estimated
+//        maximum age-related heart rate would be calculated as 220 – 50 years = 170 beats per minute (bpm).
+//        The 64% and 76% levels would be:
+//        64% level: 170 x 0.64 = 109 bpm, and
+//        76% level: 170 x 0.76 = 129 bpm
+//        This shows that moderate-intensity physical activity for a 50-year-old person will
+//        require that the heart rate remains between 109 and 129 bpm during physical activity
+
+
+        if (data.getPulseRate() == null) {
+            alerts.add(
+                    Alert.builder()
+                            .alertType(AlertType.DEVICE)
+                            .gravity(Gravity.MEDIUM)
+                            .message("Pulse is not available. Check for device errors.")
+                            .build()
+            );
+        } else {
+            double maximumAgeRelatedHeartRate = 220.0 - Double.parseDouble(data.getAge());
+            double percent64 = (64.0 * maximumAgeRelatedHeartRate) / 100.0;
+            double percent76 = (76.0 * maximumAgeRelatedHeartRate) / 100.0;
+            if (Integer.parseInt(data.getPulseRate()) > percent76) {
+                alerts.add(
+                        Alert.builder()
+                                .alertType(AlertType.PULSE)
+                                .gravity(Gravity.HIGH)
+                                .message("Pulse is too high. Recommended for moderate-intensity physical activity is between " + percent64 + " and " + percent76 + ".")
+                                .build()
+                );
+            } else if (Integer.parseInt(data.getPulseRate()) < percent64) {
+                alerts.add(
+                        Alert.builder()
+                                .alertType(AlertType.PULSE)
+                                .gravity(Gravity.HIGH)
+                                .message("Pulse is too low. Recommended for moderate-intensity physical activity is between " + percent64 + " and " + percent76 + ".")
+                                .build()
+                );
+            }
+        }
+//        Normal body temperature can range from to 36.5 degrees C 37.2 degrees C for a healthy adult.
+
+        if (data.getBodyTemperature() == null) {
+            alerts.add(
+                    Alert.builder()
+                            .alertType(AlertType.DEVICE)
+                            .gravity(Gravity.MEDIUM)
+                            .message("Body temperature is not available. Check for device errors.")
+                            .build()
+            );
+        } else {
+            if (Double.parseDouble(data.getBodyTemperature()) > 37.2) {
+                alerts.add(
+                        Alert.builder()
+                                .alertType(AlertType.BODY_TEMPERATURE)
+                                .gravity(Gravity.HIGH)
+                                .message("Body temperature is too high.")
+                                .build()
+                );
+            } else if (Double.parseDouble(data.getBodyTemperature()) < 36.5) {
+                alerts.add(
+                        Alert.builder()
+                                .alertType(AlertType.BODY_TEMPERATURE)
+                                .gravity(Gravity.HIGH)
+                                .message("Body temperature is too low.")
+                                .build()
+                );
+            }
+        }
+
+//        Blood pressure is categorized as normal, elevated, or stage 1 or stage 2 high blood pressure:
+//        Normal blood pressure is systolic of less than 120 and diastolic of less than 80 (120/80)
+//        Elevated blood pressure is systolic of 120 to 129 and diastolic less than 80
+//        Stage 1 high blood pressure is systolic is 130 to 139 or diastolic between 80 to 89
+//        Stage 2 high blood pressure is when systolic is 140 or higher or the diastolic is 90 or higher
+
+        if (data.getBloodPressure() == null) {
+            alerts.add(
+                    Alert.builder()
+                            .alertType(AlertType.DEVICE)
+                            .gravity(Gravity.MEDIUM)
+                            .message("Blood pressure is not available. Check for device errors.")
+                            .build()
+            );
+        } else {
+            String[] tokens = data.getBloodPressure().split("/");
+            int systolic = Integer.parseInt(tokens[0]);
+            int diastolic = Integer.parseInt(tokens[1]);
+            if (!(systolic <= 120 && diastolic < 80)) {
+                if (systolic > 120 && systolic <= 129 && diastolic <= 80) {
+                    alerts.add(
+                            Alert.builder()
+                                    .alertType(AlertType.BLOOD_PRESSURE)
+                                    .gravity(Gravity.MEDIUM)
+                                    .message("Blood pressure is elevated.")
+                                    .build()
+                    );
+                } else if ((systolic >= 130 && systolic <= 139) && (diastolic >= 80 && diastolic <= 89)) {
+                    alerts.add(
+                            Alert.builder()
+                                    .alertType(AlertType.BLOOD_PRESSURE)
+                                    .gravity(Gravity.HIGH)
+                                    .message("Blood pressure is in stage 1.")
+                                    .build()
+                    );
+                } else if ((systolic >= 140) || (diastolic >= 90)) {
+                    alerts.add(
+                            Alert.builder()
+                                    .alertType(AlertType.BLOOD_PRESSURE)
+                                    .gravity(Gravity.HIGH)
+                                    .message("Blood pressure is in stage 2.")
+                                    .build()
+                    );
+                }
+            }
+        }
+
+//        Normal pulse oximeter readings usually range from 95 to 100 percent.
+//        Values under 90 percent are considered low.
+        if (data.getOxygenLevel() == null) {
+            alerts.add(
+                    Alert.builder()
+                            .alertType(AlertType.DEVICE)
+                            .gravity(Gravity.MEDIUM)
+                            .message("Oxygen level is not available. Check for device errors.")
+                            .build()
+            );
+        } else {
+            int oxygenLevel = Integer.parseInt(data.getOxygenLevel());
+            if (oxygenLevel < 90) {
+                alerts.add(
+                        Alert.builder()
+                                .alertType(AlertType.OXYGEN_IN_BLOOD)
+                                .gravity(Gravity.HIGH)
+                                .message("Oxygen level is low.")
+                                .build()
+                );
+            }
+        }
+
+        return Mono.just(data.toBuilder().alerts(alerts).build());
     }
 
     private Mono<Patient> persistDetails(Patient patient, PatientDTO patientDTO) {
--- src/main/java/com/example/healthmonitoring/internal/vitalsigns/VitalSignsController.java
+++ src/main/java/com/example/healthmonitoring/internal/vitalsigns/VitalSignsController.java
@@ -1,14 +1,19 @@
 package com.example.healthmonitoring.internal.vitalsigns;
 
+import com.example.healthmonitoring.common.domain.entity.utility.PatientVitalSignsData;
+import com.example.healthmonitoring.internal.supervisor.security.utils.AuthenticationUtils;
+import com.example.healthmonitoring.internal.supervisor.service.SupervisorService;
 import com.example.healthmonitoring.internal.vitalsigns.dto.VitalSignsDTO;
 import com.example.healthmonitoring.internal.vitalsigns.service.VitalSignsService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -18,12 +23,18 @@ import reactor.core.publisher.Mono;
 public class VitalSignsController {
 
     VitalSignsService vitalSignsService;
+    SupervisorService supervisorService;
 
     @PostMapping("/vital-signs")
     public Mono<Void> ingestVitalSigns(@RequestBody VitalSignsDTO vitalSignsDTO) {
         log.info("Trying to ingest vitals {}", vitalSignsDTO);
-
         return vitalSignsService.persistVitalSigns(vitalSignsDTO);
+    }
+
+    @GetMapping("/patients")
+    public Flux<PatientVitalSignsData> patientVitalSignsDataFlux() {
+        log.info("Returining patients data for supervisor {}", AuthenticationUtils.getLoggedInUser().getId());
+        return supervisorService.getPatientsData(AuthenticationUtils.getLoggedInUser());
     }
 
 }
