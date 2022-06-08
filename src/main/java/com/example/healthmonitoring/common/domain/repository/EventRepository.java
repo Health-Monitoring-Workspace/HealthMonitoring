f5178cb181f821f7a21428f5a26d52a868046975
Daniel Burtic <patricia.burtic@gmail.com>
2022-06-08 15:41:25 +0300

    Delete data older than 90 days

--- src/main/java/com/example/healthmonitoring/common/domain/repository/EventRepository.java
+++ src/main/java/com/example/healthmonitoring/common/domain/repository/EventRepository.java
@@ -4,6 +4,7 @@ import com.example.healthmonitoring.common.domain.entity.Event;
 import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.scheduling.annotation.Scheduled;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -15,5 +16,9 @@ public interface EventRepository extends ReactiveCrudRepository<Event, UUID> {
     Flux<Event> getRecentEventsForPatient(final @Param("patientId") UUID patientId);
 
     Mono<Void> deleteAllByDeviceId(UUID deviceId);
+
+    @Query("delete from events e where e.created_at::timestamp - now()::timestamp + 1 < 90")
+    @Scheduled(cron = "59 23 * * *")
+    Mono<Void> deleteDataOlderThan90Days();
 
 }
